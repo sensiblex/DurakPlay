6e9808118be50ed95ccedfba0dd1029bb0e02265
Alexander <panfilov.alexander@bk.ru>
2025-06-08 17:09:01 +0300

    Changes

--- TestBaseFunctions.py
+++ TestBaseFunctions.py
@@ -3,7 +3,9 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+        print('Testing something')
         self.assertEqual(True, False)  # add assertion here
+
 
 
 if __name__ == '__main__':
